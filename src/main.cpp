75d8c30858aa931106b246a584fdb19fa4ab488f
Allisson128 <Allisson128@users.noreply.github.com>
2014-06-24 15:00:45 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <list>
-#include "Pagina.cpp"
+#include <vector>
+// #include "Page.cpp"
+#include "BufferPool.cpp"
 
-using namespace std;
 
 int main() {
 
-  list<Slot> l;
+  BufferPool bp;
+  FILE *file;
+
+  file = fopen("txt/R62500.txt", "r");
+
+  aloca_pagina(bp);
+  le_pagina(bp, file, 0);
+  // mostra_paginas(bp);
+  // cout << "size: " << bp.frames.size() << endl;
+
   return 0;
 }
