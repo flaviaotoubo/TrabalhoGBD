8ef0a2670f29c3dfb16b678ab3f1a662ff59a0c2
Allisson128 <Allisson128@users.noreply.github.com>
2014-07-17 09:02:53 -0300

    Update Page.cpp

--- src/Page.cpp
+++ src/Page.cpp
@@ -3,6 +3,7 @@
 #include <list>
 #include <vector>
 #include <cstring>
+#include <cstdlib>
 
 #define MAX_SIZE 10240		// 10 KB = 10240 Bytes = 512 registros/pagina
 
@@ -11,14 +12,6 @@ using namespace std;
 // VARIAVEIS GLOBAIS
 int g_id = 0;
 int CURRENT_SIZE = 512; //reg/ pag; controle do tamanho atual da pagina
-
-struct Register {		// 20 Bytes
-  int nroPagina;		//  4 Bytes
-  int nroSlot;			//  4 Bytes
-  char atributo1[10]; 		// 10 Bytes
-  char atributo2[10]; 		// 10 Bytes
-};
-
 
 struct Slot {			// 8 Bytes
   int pos;			// 4 Bytes
@@ -29,6 +22,13 @@ struct DirSlots {		// 8 + 8 * nroSlots Bytes
   int nroSlots;  		// 4 Bytes
   int freeSpace; 		// 4 Bytes
   vector<Slot> l;			// 8 * nroSlots Bytes // sizeof = 16
+};
+
+struct Register {		// 20 Bytes
+  int nroPagina;		//  4 Bytes
+  int nroSlot;			//  4 Bytes
+  char atributo1[11]; 		// 10 Bytes
+  char atributo2[11]; 		// 10 Bytes
 };
 
 struct Page {
@@ -46,25 +46,27 @@ DirSlots cria_diretorio_slots() {
   return ds;
 }
 
-Page cria_pagina() {
-  Page p;
-  p.id = g_id++;
-  p.ds = cria_diretorio_slots();
+Page* cria_pagina() {
+
+  Page* p = (Page*) malloc (sizeof(Page));
+
+  p->id = g_id++;
+  p->ds = cria_diretorio_slots();
+
   return p;
 }
 
 
-void aloca_registro(Page pagina, char* dados1, char* dados2) {
+void aloca_registro(Page* pagina, char* dados1, char* dados2) {
 
   Register reg;
 
-  reg.nroPagina = pagina.id;
-  reg.nroSlot = pagina.ds.nroSlots++;
+  reg.nroPagina = pagina->id;
+  reg.nroSlot = pagina->ds.nroSlots++;
 
   strcpy(reg.atributo1, dados1);
   strcpy(reg.atributo2, dados2);
 
-  pagina.registros.push_back(reg);;
+  pagina->registros.push_back(reg);;
   
 }
-
